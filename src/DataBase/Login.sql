f207cc6541f66d3217ecc57bfe9f87917c477079
Emon-2004 <s0172401848@gmail.com>
2026-06-25 00:30:27 +0600

    Fixed Database

--- src/DataBase/Login.sql
+++ src/DataBase/Login.sql
@@ -1,25 +1,13 @@
-DROP TABLE IF EXISTS Client CASCADE;
 DROP TABLE IF EXISTS Registration CASCADE;
 
 CREATE TABLE Registration(
-	Registration_ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	First_name TEXT NOT NULL,
-	Last_name TEXT NOT NULL,
-	Client_Email TEXT NOT NULL UNIQUE,
-	password TEXT NOT NULL,
-	
-CONSTRAINT chk_reg_email CHECK (Client_Email LIKE '%@%.%'),
-CONSTRAINT chk_reg_password CHECK (LENGTH(password) >= 8),
-CONSTRAINT fk_client_reg FOREIGN KEY (Client_Email)
-                             REFERENCES Client (Email)
-                             ON DELETE CASCADE
-
-);
-
-CREATE TABLE Client(
-	Email TEXT NOT NULL PRIMARY KEY,
+	Reg_ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	Full_name TEXT NOT NULL,
+	Email TEXT NOT NULL UNIQUE,
+	Phone_Number TEXT UNIQUE,
 	Password TEXT NOT NULL,
 
-CONSTRAINT chk_client_email    CHECK (Email LIKE '%@%.%'),
-CONSTRAINT chk_client_password CHECK (LENGTH(Password) >= 8)
+	CONSTRAINT chk_client_email    CHECK (Email LIKE '%@%.%'),
+	CONSTRAINT chk_client_password CHECK (LENGTH(Password) >= 8),
+	CONSTRAINT chk_client_phone    CHECK (Phone_Number ~ '^\+?[0-9\-\s]{7,15}$')
 );
